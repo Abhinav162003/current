e514fb6c9e6b0db7144966699d3f12a28b7762fc
Anshumanv36 <anshumanmishra.v36@gmail.com>
2024-08-29 22:38:12 +0530

    crossword improvements

--- src/screens/Crossword/Grid.js
+++ src/screens/Crossword/Grid.js
@@ -1,18 +1,53 @@
+// import React from "react";
+// import { View, StyleSheet } from "react-native";
+// import Cell from "./Cell";
+
+// const Grid = ({ grid, inputs, onChange }) => {
+//   return (
+//     <View style={styles.gridContainer}>
+//       {grid.map((row, rowIndex) => (
+//         <View key={rowIndex} style={styles.row}>
+//           {row.map((cell, colIndex) => (
+//             <Cell
+//               key={colIndex}
+//               cell={cell}
+//               value={inputs[rowIndex][colIndex]}
+//               onChange={(text) => onChange(rowIndex, colIndex, text)}
+//             />
+//           ))}
+//         </View>
+//       ))}
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   gridContainer: {
+//     flexDirection: "column",
+//   },
+//   row: {
+//     flexDirection: "row",
+//   },
+// });
+
+// export default Grid;
+
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import Cell from "./Cell";
 
-const Grid = ({ grid, inputs, onChange }) => {
+const Grid = ({ grid, inputs, onChange, correctAnswers }) => {
   return (
-    <View style={styles.gridContainer}>
+    <View style={styles.grid}>
       {grid.map((row, rowIndex) => (
-        <View key={rowIndex} style={styles.row}>
+        <View style={styles.row} key={rowIndex}>
           {row.map((cell, colIndex) => (
             <Cell
-              key={colIndex}
+              key={`${rowIndex}-${colIndex}`}
               cell={cell}
               value={inputs[rowIndex][colIndex]}
-              onChange={(text) => onChange(rowIndex, colIndex, text)}
+              onChange={(value) => onChange(rowIndex, colIndex, value)}
+              editable={!!correctAnswers[rowIndex][colIndex]} // Editable only if there's an answer
             />
           ))}
         </View>
@@ -22,7 +57,7 @@ const Grid = ({ grid, inputs, onChange }) => {
 };
 
 const styles = StyleSheet.create({
-  gridContainer: {
+  grid: {
     flexDirection: "column",
   },
   row: {
